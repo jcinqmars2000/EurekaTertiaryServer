0e92c02607ff1ed6a5a3ea3c2463703d6910154f
John Cinq-Mars <jcinqmars2000@yahoo.com>
2019-01-17 09:26:02 -0500

    Initial commit

---
+++ src/main/java/com/responsible_nrg/EurekaTertiaryServerApplication.java
@@ -0,0 +1,16 @@
+package com.responsible_nrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class EurekaTertiaryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaTertiaryServerApplication.class, args);
+	}
+}
